baf85cdd46819f3bf276b17eb6148c93e9d09509
Nicolas Neven <nneven@usc.edu>
2021-04-25 00:40:58 -0700

    add coingecko api

--- projects/nerve.js
+++ projects/nerve.js
@@ -2,61 +2,61 @@
   Modules
   ==================================================*/
 
-  const sdk = require('@defillama/sdk');
-  const BigNumber = require('bignumber.js');
-  const _ = require('underscore');
+const sdk = require('@defillama/sdk');
+const BigNumber = require('bignumber.js');
+// const _ = require('underscore');
+const axios = require("axios");
 
 /*==================================================
   Addresses
   ==================================================*/
 
-  // const xNRV = "0x15B9462d4Eb94222a7506Bc7A25FB27a2359291e"
-  const threeNRV = "0x1B3771a66ee31180906972580adE9b81AFc5fCDc"
-  // const nrvBTC = "0xD1D5Af92C606C6F2eC59D453f57A6FCc188D7dB5"
-  // const nrvETH = "0x0d283BF16A9bdE49cfC48d8dc050AF28b71bdD90"
+/*
+// const xNRV = 
+const threeNRV = "0x1B3771a66ee31180906972580adE9b81AFc5fCDc"
+// const nrvBTC = 
+// const nrvETH = 
 
-  const tokens = {
-  	// NRV
-  	// "0x42f6f551ae042cbe50c739158b4f0cac0edb9096": [xNRV],
-  	// BUSD
-  	"0xe9e7cea3dedca5984780bafc599bd69add087d56": [threeNRV],
-  	// USDC
-  	"0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d": [threeNRV],
-  	// USDT
-  	"0x55d398326f99059ff775485246999027b3197955": [threeNRV],
-  	// anyBTC
-  	// "0x54261774905f3e6e9718f2abb10ed6555cae308a": [nrvBTC],
-  	// anyETH
-  	// "0x6f817a0ce8f7640add3bc0c1c2298635043c2423": [nrvETH],
-  }
+const tokens = {
+	// NRV
+	// "0x42f6f551ae042cbe50c739158b4f0cac0edb9096"
+	// BUSD
+	"0xe9e7cea3dedca5984780bafc599bd69add087d56": [threeNRV],
+	// USDC
+	"0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d": [threeNRV],
+	// USDT
+	"0x55d398326f99059ff775485246999027b3197955": [threeNRV],
+}
+*/
 
 /*==================================================
   TVL
   ==================================================*/
 
-	let balances = {};
-    let calls = [];
-    
-    for (const token in tokens) {
-      for(const poolAddress of tokens[token])
-      calls.push({
-        target: token,
-        params: poolAddress
-      })
-    }
+let balances = {};
 
-	const ZERO = new BigNumber(0)
-	const ETHER = new BigNumber(10).pow(18)
+/*
+let calls = [];
+for (const token in tokens) {
+  for(const poolAddress of tokens[token])
+  calls.push({
+    target: token,
+    params: poolAddress
+  });
+}
+*/
 
-	async function tvl(timestamp) {
+async function tvl(timestamp) {
+	/*
 	const { block } = await sdk.api.util.lookupBlock(timestamp, {
-	    chain: 'bsc'
-	})
+	  chain: 'bsc'
+	});
+
 	const balanceOfResults = (await sdk.api.abi.multiCall({
-	    calls: calls,
-	    abi: 'erc20:balanceOf',
-	    chain: 'bsc'
-	}))
+	  calls: calls,
+	  abi: 'erc20:balanceOf',
+	  chain: 'bsc'
+	}));
 
 	_.each(balanceOfResults.output, (balanceOf) => {
 	  if(balanceOf.success) {
@@ -64,6 +64,17 @@
 	    balances['bsc:' + address] = BigNumber(balances[address] || 0).plus(balanceOf.output).toFixed()
 	  }
 	});
+	*/
+
+	// use BUSD stable as total sum denomination 
+	const address = 'bsc:0xe9e7cea3dedca5984780bafc599bd69add087d56';
+	// CoinGecko API
+	const data = await axios.get('https://api.coingecko.com/api/v3/coins/nerve-finance')
+  	  .then((response) => {
+  		  const tvl = BigNumber(response.data['market_data']['total_value_locked']['usd']);
+  		  // return value expects 18 decimals
+		  balances[address] = tvl.shiftedBy(18);
+  	});
 
 	return balances;
 }
